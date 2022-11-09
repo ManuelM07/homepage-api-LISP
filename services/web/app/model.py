e413dec58695e1f78f35b4260516b245baf81ffd
ManuelM07 <83485551+ManuelM07@users.noreply.github.com>
2022-11-08 22:52:48 -0500

    Update model.py
    Add UserMixin and create new parameter

--- services/web/app/model.py
+++ services/web/app/model.py
@@ -1,10 +1,11 @@
-from app import app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
+from flask_login import UserMixin
+from flask import Flask
 import enum
 
+app = Flask(__name__)
 db = SQLAlchemy(app)
-
 
 class Role(enum.Enum):
     admin = "admin"
@@ -12,10 +13,11 @@ class Role(enum.Enum):
     coach = "coach"
     health_professional = "health_professional"
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
+    social_id = db.Column(db.String(250))
     dni = db.Column(db.String(250), unique=True, nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
@@ -28,6 +30,19 @@ class User(db.Model):
     role = db.Column(db.Enum(Role), nullable=False)
     user_zone = relationship("UserZone")
 
+    def is_active(self):
+        return super().is_active
+
+    def is_authenticated(self):
+        return super().is_authenticated
+    
+    def is_anonymous(self):
+        return super().is_anonymous
+
+    def get_id(self):
+        return super().get_id()
+
+
 class Zone(db.Model):
     __tablename__ = 'zone'
 
@@ -35,6 +50,7 @@ class Zone(db.Model):
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.Text, nullable=False)
     user_zone = relationship("UserZone")
+
 
 class UserZone(db.Model):
     __tablename__ = 'user_zone'
